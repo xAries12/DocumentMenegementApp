20fb07827516ba3ba893b268d0595fb0a052f3cc
xAries12 <97310037+xAries12@users.noreply.github.com>
2024-05-28 12:35:26 +0300

    DomcumentManegementAPI created using spring initializar with Spring Web and Spring JPA Data

---
+++ DocumentMenegementAPI/src/main/java/com/dragomir/DocumentMenegementApplication.java
@@ -0,0 +1,13 @@
+package com.dragomir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentMenegementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentMenegementApplication.class, args);
+	}
+
+}
